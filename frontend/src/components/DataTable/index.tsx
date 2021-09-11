5f2ee0caead2f5336f8215354a6292d4f97259fe
Rafael Rios <rafaelmaiarios@hotmail.com>
2021-09-11 11:34:48 -0300

    Static charts

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -15,21 +15,21 @@ const DataTable = () => {
         <tbody>
             <tr>
                 <td>22/04/2021</td>
-                <td>Jadson rios</td>
+                <td>Barry Allen</td>
                 <td>34</td>
                 <td>25</td>
                 <td>15017.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Sandra Maia Rios</td>
+                <td>Barry Allen</td>
                 <td>34</td>
                 <td>25</td>
                 <td>15017.00</td>
             </tr>
             <tr>
                 <td>22/04/2021</td>
-                <td>Rafael Maia</td>
+                <td>Barry Allen</td>
                 <td>34</td>
                 <td>25</td>
                 <td>15017.00</td>
